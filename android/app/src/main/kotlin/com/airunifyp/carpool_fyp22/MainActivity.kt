70179d4eb7966624f3e8049ff5e3a2177fa61376
CoDrift <co.drift07@gmail.com>
2023-05-17 17:29:01 +0500

    init

---
+++ android/app/src/main/kotlin/com/airunifyp/carpool_fyp22/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.airunifyp.carpool_fyp22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AccountManagement/Intro.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(),
+    );
+  }
+}
